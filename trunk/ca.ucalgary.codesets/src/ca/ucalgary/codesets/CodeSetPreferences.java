8468d3864b0db30d3fb00cbf1a1161e65cc5448b
sillito <sillito@abcca570-592f-0410-9db0-014be2e416ff>
2007-05-22 21:46:16 +0000

    [for Jamie] makes preference changes take immediate effect (i.e., without waiting for the OK button).

--- trunk/ca.ucalgary.codesets/src/ca/ucalgary/codesets/CodeSetPreferences.java
+++ trunk/ca.ucalgary.codesets/src/ca/ucalgary/codesets/CodeSetPreferences.java
@@ -1,11 +1,7 @@
 package ca.ucalgary.codesets;
 
 import java.util.ArrayList;
-
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.dialogs.MessageDialogWithToggle;
-import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -14,47 +10,42 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Slider;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.preferences.ViewSettingsDialog;
 
 import ca.ucalgary.codesets.sets.CodeSet;
 
-
-
+/**
+ * The CodeSetPreferences a set of code designed to allow the selection, and activation or deactivation
+ * of a given set or sets.
+ * @author starkej
+ *
+ */
 public class CodeSetPreferences extends MessageDialog {
 
 	private ArrayList<CodeSet> sets;
-	private String[] buttonNames = {"History Set", "Change Set"};
-	private ArrayList<CodeSet> modified = new ArrayList<CodeSet>();
 	
+	/**
+	 * CodeSetPreferences is used for controlling all of the active sets that run at a given
+	 * time.  
+	 * @param parentShell
+	 * @param sets
+	 */
 	public CodeSetPreferences (Shell parentShell, ArrayList<CodeSet> sets) {
 		super( parentShell,
                 "Set Preferences",
                 null,
                 "Select the sets you would like to use.",
                 0,
-                new String[]{IDialogConstants.OK_LABEL, IDialogConstants.CANCEL_LABEL},
+                new String[]{"Done"},
                 0);
 		this.sets = sets;
 	}
 	
 	/**
-	 * Modifies the preferences on buttons pressed.
+	 * Sets the function for the done button to close the window
 	 * @param pButtonID The id for the button pressed.
 	 */
 	protected void buttonPressed( int pButtonID ) 
 	{
-		System.out.println(modified.size());
-		if( pButtonID == 0 )
-		{
-			for (CodeSet m:modified) {
-				if (m.isActivated())
-					m.deactivate();
-				else
-					m.activate();
-			}
-		}
 		close();
 	}
 
@@ -66,6 +57,7 @@ public class CodeSetPreferences extends MessageDialog {
 	protected Control createCustomArea( Composite parent )
 	{
 		ArrayList<Button> buttons = new ArrayList<Button>();
+		//Create a toggle for each set
 		for (CodeSet s:sets)
 		{
 			buttons.add(createToggleButton(parent, s));
@@ -73,8 +65,20 @@ public class CodeSetPreferences extends MessageDialog {
 		return super.createCustomArea( parent );
 	}
 	
+	/**
+	 * Toggles the activated state of a set.  For instance, if a set is activated, it will
+	 * deactivate that set
+	 * @param set
+	 */
+	protected void toggle(CodeSet set) {
+		if (set.isActivated())
+			set.deactivate();
+		else
+			set.activate();
+	}
+	
 	protected Button createToggleButton(Composite parent, final CodeSet s) {
-        final Button button = new Button(parent, SWT.CHECK | SWT.LEFT);
+        Button button = new Button(parent, SWT.CHECK | SWT.LEFT);
         
         GridData data = new GridData(SWT.NONE);
         data.horizontalSpan = 2;
@@ -83,14 +87,10 @@ public class CodeSetPreferences extends MessageDialog {
         button.setText(s.getAction().getText());
         button.setSelection(s.isActivated());
 
+        // Listens for selection of widgets, in this case, ToggleButtons
         button.addSelectionListener(new SelectionAdapter() {
-
             public void widgetSelected(SelectionEvent e) {
-                if (modified.contains(s)) {
-                	modified.remove(s);
-                }
-                else
-                	modified.add(s);
+            	toggle(s);
            }
         });
 
