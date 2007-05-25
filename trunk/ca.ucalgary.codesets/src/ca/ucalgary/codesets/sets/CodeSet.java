71ae995a0c41249dee851e0aa32e05b6cd2455af
jrstarke <jrstarke@abcca570-592f-0410-9db0-014be2e416ff>
2007-05-25 16:17:15 +0000

    for Curtis: Gives each set its own ordering. Changes set orderings to Chronological, except the SearchSet which is alphabetical. Fixed the intersection between sets text colour

--- trunk/ca.ucalgary.codesets/src/ca/ucalgary/codesets/sets/CodeSet.java
+++ trunk/ca.ucalgary.codesets/src/ca/ucalgary/codesets/sets/CodeSet.java
@@ -1,7 +1,9 @@
 package ca.ucalgary.codesets.sets;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.EventListener;
+import java.util.HashSet;
 
 import org.eclipse.jdt.core.ISourceReference;
 import org.eclipse.jface.action.Action;
@@ -12,7 +14,7 @@ import ca.ucalgary.codesets.ResultSet;
 import ca.ucalgary.codesets.listeners.CodeSetListener;
 import ca.ucalgary.codesets.listeners.SetListener;
 
-public class CodeSet extends ArrayList<ISourceReference> implements IStructuredContentProvider {
+public class CodeSet extends HashSet<SetNode> implements IStructuredContentProvider {
 
 	private int sizeOfList = 10;
 	protected CodeSetListener listener;
@@ -45,26 +47,76 @@ public class CodeSet extends ArrayList<ISourceReference> implements IStructuredC
 			listensToUs.refresh(this);
 	}
 	
-	// This code below is not optimized, and might be slowing the code down.
-	public boolean add(ISourceReference isr) {
+	/**
+	 * Checks the hashSet to see if it contains the given ISourceReference, if it does contain it
+	 * already, it will update the time the ISourceReference was added to the set. 
+	 * If it does not contain the ISourceReference, then it will add a new SetNode.
+	 * @param isr
+	 * @return true
+	 */
+	public boolean add(ISourceReference isr) {		
 		if(contains(isr))
-			remove(isr);  				//delete the object if it is in the list already
-		
-		add(0,isr);						//add the object to the front of the list
-		if(size() > sizeOfList)		//remove the last unit if the size is greater than allowed
-			remove(sizeOfList-1);		
+			updateTime(isr);//change the date not add a new isr
+		else
+			super.add(new SetNode(isr));
 		changed();
 		return true;
+	}
+	
+	/**
+	 * This function searches through the hashSet for the give ISourceReference, and 
+	 * when it finds it, it replaces (updates) the time to the current time.
+	 * @param isr
+	 */
+	private void updateTime(ISourceReference isr) {
+		java.util.Iterator<SetNode> i = iterator();
+		
+		while(i.hasNext()) {
+			SetNode sn = i.next();
+			if(sn.getIsourcereference() == isr) {
+				sn.setTime(System.currentTimeMillis());
+				return;
+			}
+		}
+	}
+	
+	/**
+	 * Checks if the provided ISourceReference already exists
+	 * @param isr
+	 * @return true if the ISourceReference exists
+	 * @return false if the ISourceReference does not exist
+	 */
+	public boolean contains(ISourceReference isr) {
+		SetNode node = new SetNode(isr);
+		return contains(node);
+	}
+	
+	
+	/*
+	 * Since the HashSet is a set of SetNodes, getting the elements will return an array 
+	 * of SetNodes which will cause an error for the viewer.
+	 * This copies the ISourceReferences from each node into a separate array and returns this array
+	 * It also sorts the array using Arrays.sort() which uses the compareTo() method in the SetNode.
+	 * This orders them chronologically
+	 * non-JavaDoc
+	 * @param parent
+	 * @return isr -> the array of ISourceReferences
+	 */
+	public Object[] getElements(Object parent) {
+		Object[] temp = toArray();
+		Arrays.sort(temp);
+		
+		ISourceReference[]  isr = new ISourceReference[temp.length];
+		for(int i = 0; i<isr.length;i++)
+			isr[i] = ((SetNode)temp[i]).getIsourcereference();
+		
+		return isr;
 	}
 	
 	public void inputChanged(Viewer v, Object oldInput, Object newInput) {
 	}
 	
 	public void dispose() {
-	}
-	
-	public Object[] getElements(Object parent) {
-		return toArray();
 	}
 	
 	// Can use this in the future if you want the size of the list to change
