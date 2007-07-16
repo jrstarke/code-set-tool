e3901fb5d5ce5b0216d8b80ea133cd40d5c40ddb
fraser.curtis <fraser.curtis@abcca570-592f-0410-9db0-014be2e416ff>
2007-07-16 22:07:15 +0000

    Author Jamie: Bug Fix: Fixed the freezing problem - Added the ability show all elements in a particular file

--- trunk/ca.ucalgary.codesets/src/ca/ucalgary/codesets/models/RefOfFile.java
+++ trunk/ca.ucalgary.codesets/src/ca/ucalgary/codesets/models/RefOfFile.java
@@ -1,5 +1,7 @@
 package ca.ucalgary.codesets.models;
 
+import java.util.List;
+
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
@@ -11,27 +13,28 @@ import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Expression;
-import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.internal.corext.dom.GenericVisitor;
+import org.eclipse.jdt.ui.JavaElementLabelProvider;
 
 //uses eclipse's ast parser to compute the set of all references from a
 //given element.
-public class ReferenceFromSearch extends GenericVisitor {
+public class RefOfFile extends GenericVisitor {
 	IMember element;
 	CodeSet set;
 
-	public void search(IJavaElement element, String name) {
-		set = new CodeSet(name, "references from");
+	public void search(IJavaElement element) {
 		this.element = (IMember)element;
 		ICompilationUnit unit = this.element.getCompilationUnit();
 		ASTParser parser= ASTParser.newParser(AST.JLS3);
 		parser.setSource(unit);
 		parser.setResolveBindings(true);
+		set = new CodeSet(new JavaElementLabelProvider().getText(unit), "elements of file");
 
 		CompilationUnit node = (CompilationUnit) parser.createAST(null);
 		node.accept(this);
@@ -43,10 +46,7 @@ public class ReferenceFromSearch extends GenericVisitor {
 		return true;
 	}
 
-	public boolean visit (ExpressionStatement node) {
-		Expression expression = node.getExpression();
-		return visitNode(node);
-	}
+
 
 	public boolean visit (MethodInvocation node) {
 		IMethodBinding binding = node.resolveMethodBinding();
@@ -56,6 +56,7 @@ public class ReferenceFromSearch extends GenericVisitor {
 		return visitNode(node);
 	}
 
+	// TODO fix exception problem found when using this block of code
 	public boolean visit (ClassInstanceCreation node) {
 		IMethodBinding binding = node.resolveConstructorBinding();
 		IJavaElement element = binding.getJavaElement();
@@ -65,23 +66,26 @@ public class ReferenceFromSearch extends GenericVisitor {
 	}
 
 	public boolean visit(MethodDeclaration node) {
-		return checkElement(node);
+		IMethodBinding binding = node.resolveBinding();
+		IJavaElement element = binding.getJavaElement();
+		if (element != null)
+			set.add((ISourceReference)element);
+		return visitNode(node);
 	}
 
-	public boolean visit (FieldDeclaration node) {
-		return checkElement(node);
-	}
-
-	// returns true if the given node is within the range of the IJavaElement we
-	// are searching on
-	public boolean checkElement (ASTNode node) {
-		try {
-			if ((node.getStartPosition() == element.getSourceRange().getOffset()) && 
-					(node.getLength() == element.getSourceRange().getLength()))
-				return visitNode(node);
-		} catch (JavaModelException e) {
-			e.printStackTrace();
+	public boolean visit(FieldDeclaration node) {
+		for (Object f: node.fragments()) {
+			if (f instanceof VariableDeclarationFragment) {
+				VariableDeclarationFragment fragment = (VariableDeclarationFragment) f;
+				IVariableBinding binding = fragment.resolveBinding();
+				IJavaElement element = binding.getJavaElement();
+				if (element != null)
+					set.add((ISourceReference)element);
+				System.out.println(element.getElementName());
+			}
 		}
-		return !visitNode(node);
+		return visitNode(node);
+
 	}
+
 }
