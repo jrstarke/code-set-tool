544a4f7eacaff04800b025435a731fd381685a55
jrstarke <jrstarke@abcca570-592f-0410-9db0-014be2e416ff>
2007-08-20 21:04:53 +0000

    Added seperator lines between the Type groupings

--- ca.ucalgary.codesets/src/ca/ucalgary/codesets/views/CombinedView.java
+++ ca.ucalgary.codesets/src/ca/ucalgary/codesets/views/CombinedView.java
@@ -23,28 +23,28 @@ import ca.ucalgary.codesets.controllers.Logger;
 import ca.ucalgary.codesets.models.NodeSet;
 import ca.ucalgary.codesets.models.NodeSetManager;
 
-// this view is the main presentation of the currently applicable set of ASTNode's.
+//this view is the main presentation of the currently applicable set of ASTNode's.
 public class CombinedView extends ViewPart  {
 	Action nameSetAction;
-	CombinedViewController cvc;
-	
+//	CombinedViewController cvc;
+
 	public void createPartControl(Composite parent) {
 		new CombinedViewController(parent);
 		makeActions();
 		createToolbar();
 	}
-	
+
 	public void setFocus() {
 	}
-	
+
 	// static methods and fields for creating the various widgets and composites used
 	// to display node sets
-	
+
 	static Color methodColor = new Color(null, 100,100,100);
 	static Color commentColor = new Color(null, 100,100,175);
 	static Color methodNameColor = new Color(null, 0,0,0);
 	static Color classNameColor = methodNameColor;
-	
+
 	static Label label(Composite parent, String text, int style, int height, Color color) {
 		Label label = new Label(parent, SWT.NONE);
 		label.setText(text);
@@ -52,7 +52,7 @@ public class CombinedView extends ViewPart  {
 		label.setForeground(color);
 		return label;
 	}
-	
+
 	static void fontStyle(Control widget, int style, int height) {
 		Font f = widget.getFont();
 		FontData[] fd = f.getFontData();
@@ -60,13 +60,16 @@ public class CombinedView extends ViewPart  {
 		fd[0].setHeight(height);
 		widget.setFont(new Font(f.getDevice(), fd));
 	}
-	
+
 	public static Composite classView(Composite parent, String text, String comment, Listener listener) {
 		Composite result = new Composite(parent, SWT.NONE);
 		RowLayout layout = new RowLayout(SWT.VERTICAL);
 		layout.marginHeight = 0;
 		layout.marginWidth = 0;
 		result.setLayout(layout);
+		Control[] siblings = parent.getChildren();
+		if (siblings.length >= 2)
+			new Label (result, SWT.SEPARATOR | SWT.HORIZONTAL);
 		label(result, text, SWT.BOLD, 13, classNameColor).addListener(SWT.MouseDoubleClick, listener);
 		return result;
 	}
@@ -77,7 +80,7 @@ public class CombinedView extends ViewPart  {
 		layout.marginWidth = 0;
 		result.setLayout(layout);
 		label(result, text, SWT.BOLD, 11, methodNameColor).addListener(SWT.MouseDoubleClick, listener);
-		
+
 		return result;
 	}
 	public static Widget methodBodyWidget(Composite parent, String text, Listener listener) {
@@ -90,34 +93,34 @@ public class CombinedView extends ViewPart  {
 		label.addListener(SWT.MouseDoubleClick, listener);
 		return label;
 	}
-	
+
 	private void makeActions() {
 		nameSetAction = new Action() {
-		public void run(){
-			InputDialog dialog = new InputDialog(null, 
-					"Set Name",
-					"Please enter a name for the new set:", "", null);
-			dialog.open();
-			String name = dialog.getValue();
-			name = name.trim();
-			if ((name != null) && (name != "")) {
-				NodeSet currentSet = NodeSetManager.instance().combinedSet();
-				currentSet.name = name;
-				currentSet.category = "named";
-				NodeSetManager.instance().addSet(currentSet);
-				Logger.instance().addEvent("Set Named " + "\t" + name);
+			public void run(){
+				InputDialog dialog = new InputDialog(null, 
+						"Set Name",
+						"Please enter a name for the new set:", "", null);
+				dialog.open();
+				String name = dialog.getValue();
+				name = name.trim();
+				if ((name != null) && (name != "")) {
+					NodeSet currentSet = NodeSetManager.instance().combinedSet();
+					currentSet.name = name;
+					currentSet.category = "named";
+					NodeSetManager.instance().addSet(currentSet);
+					Logger.instance().addEvent("Set Named " + "\t" + name);
+				}
 			}
-		}
-	};
-	
-	nameSetAction.setToolTipText("Name this Set");  //change this for specified tooltip
-	nameSetAction.setText("Name this Set");
-	nameSetAction.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-			getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));  //image of action
+		};
 
-		
+		nameSetAction.setToolTipText("Name this Set");  //change this for specified tooltip
+		nameSetAction.setText("Name this Set");
+		nameSetAction.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
+				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));  //image of action
+
+
 	}
-	
+
 	private void createToolbar() {
 		IToolBarManager mgr = getViewSite().getActionBars().getToolBarManager();
 		mgr.add(nameSetAction);
