1747db6a54f703e44da983f4231e4b6490af4b62
sillito <sillito@abcca570-592f-0410-9db0-014be2e416ff>
2007-08-27 22:01:51 +0000

    Fixed nullpointer exception

--- ca.ucalgary.codesets/src/ca/ucalgary/codesets/views/CombinedView.java
+++ ca.ucalgary.codesets/src/ca/ucalgary/codesets/views/CombinedView.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.dialogs.InputDialog;
@@ -35,6 +36,7 @@ public class CombinedView extends ViewPart  {
 	Action incDefAction;
 	Action decDefAction;
 	CombinedViewController cvc;
+	static ElementLabelProvider labelProvider = new ElementLabelProvider();
 
 	public void createPartControl(Composite parent) {
 		cvc = new CombinedViewController(parent);
@@ -49,7 +51,7 @@ public class CombinedView extends ViewPart  {
 	// to display node sets
 
 	static Color methodColor = new Color(null, 140,140,140);
-	static Color commentColor = new Color(null, 140,140,175);
+	static Color commentColor = new Color(null, 175,140,140);
 	static Color methodNameColor = new Color(null, 0,0,0);
 	static Color classNameColor = methodNameColor;
 	static Color classBGColor = new Color(null, 250,250,200);
@@ -62,6 +64,14 @@ public class CombinedView extends ViewPart  {
 		return label;
 	}
 
+//	static Composite iconAndText(Composite parent, String text, Image i) {
+//		Composite result = new Composite(parent, SWT.NONE);
+//		Label icon = new Label(result, SWT.NONE);
+//		label(result, text, SWT.BOLD, 11, classNameColor);
+//		icon.setImage(i);
+//		return result;
+//	}
+	
 	static void fontStyle(Control widget, int style, int height) {
 		Font f = widget.getFont();
 		FontData[] fd = f.getFontData();
@@ -70,15 +80,17 @@ public class CombinedView extends ViewPart  {
 		widget.setFont(new Font(f.getDevice(), fd));
 	}
 
-	public static Composite classView(Composite parent, String text, String comment) { //, Listener listener) {
+	public static Composite classView(Composite parent, IType element, String comment) { //, Listener listener) {
 		Composite result = new Composite(parent, SWT.NONE);
 		RowLayout layout = new RowLayout(SWT.VERTICAL);
 		layout.marginHeight = 0;
 		layout.marginWidth = 0;
 		layout.spacing = 0;
 		result.setLayout(layout);
+
+		String text = labelProvider.getText(element);
 		Label label = label(result, text, SWT.BOLD, 11, classNameColor); //.addListener(SWT.MouseDoubleClick, listener);
-		label.setBackground(classBGColor);
+//		label.setBackground(classBGColor);
 		return result;
 	}
 	public static Composite methodView(Composite parent, String text, Listener listener) {
@@ -87,9 +99,8 @@ public class CombinedView extends ViewPart  {
 		layout.marginHeight = 0;
 		layout.marginWidth = 0;
 		layout.spacing = 0;
-//		layout.fill = true;
 		result.setLayout(layout);
-		label(result, text, SWT.BOLD, 11, methodNameColor).addListener(SWT.MouseDoubleClick, listener);
+		label(result, text, SWT.NORMAL, 11, methodNameColor).addListener(SWT.MouseDoubleClick, listener);
 		return result;
 	}
 	public static Composite fieldView(Composite parent, String text, Listener listener) {
@@ -100,7 +111,7 @@ public class CombinedView extends ViewPart  {
 		layout.spacing = 0;
 //		layout.fill = true;
 		result.setLayout(layout);
-		label(result, text, SWT.BOLD, 11, methodNameColor).addListener(SWT.MouseDoubleClick, listener);
+		label(result, text, SWT.NORMAL, 11, methodNameColor).addListener(SWT.MouseDoubleClick, listener);
 		return result;
 	}
 	public static Widget methodBodyWidget(Composite parent, String text, Listener listener) {
@@ -109,7 +120,7 @@ public class CombinedView extends ViewPart  {
 		return label;
 	}
 	public static Widget commentLabel(Composite parent, String text, Listener listener){
-		Label label = label(parent, text, SWT.NORMAL, 11, commentColor);
+		Label label = label(parent, text, SWT.ITALIC, 11, commentColor);
 		if(listener != null)
 			label.addListener(SWT.MouseDoubleClick, listener);
 		return label;
